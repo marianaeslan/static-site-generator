0d1e485abf15fc697425bc3748823558c4249809
marianaeslan <mariana.eslan@gmail.com>
2024-11-18 10:13:31 -0500

    chore: Clean up template files
    - Remove template content from HTML file
    - Remove template content from CSS file
    - Prepare clean slate for markdown-to-html implementation

--- src/textnode.py
+++ src/textnode.py
@@ -5,8 +5,8 @@ class TextType(Enum):
     BOLD = "bold"
     ITALIC = "italic"
     CODE = "code"
-    LINKS = "links"
-    IMAGES = "images"
+    LINK = "link"
+    IMAGE = "image"
 
 class TextNode:
     def __init__ (self, text, text_type, url=None):
@@ -15,9 +15,11 @@ class TextNode:
         self.url = url
 
     def __eq__(self, other):
-        return (self.text == other.text) and (
-            self.text_type == other.text_type) and (
-            self.url == other.url)
+        return (
+            self.text == other.text and 
+            self.text_type == other.text_type and 
+            self.url == other.url
+        )
         
     def __repr__(self) -> str:
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
