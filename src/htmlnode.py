a173b58ba27febf6868501b2007cd3c99e4f60a8
marianaeslan <mariana.eslan@gmail.com>
2024-11-19 11:40:00 -0500

    feat: Implement LeafNode with HTML rendering functionality
    - Created LeafNode class inheriting from HTMLNode, forbidding child nodes
    - Added to_html method to render HTML strings based on tag, value, and props
    - Managed edge cases for handling nodes without tag or value appropriately
    - Completed and verified functionality with unit tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -13,8 +13,24 @@ class HTMLNode:
             return ""
         s = ""
         for k, v in self.props.items(): 
-            s += f' {k}="{v}" '
+            s += f' {k}="{v}"'
         return s
     
     def __repr__(self):
         return f"HTMLNode(\ntag = {self.tag}, \nvalue = {self.value}, \nchildren = {self.children}, \nprops = {self.props}\n)"
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("All leaf nodes must have a value")
+        if self.tag is None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
+        
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,9 +1,9 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def setUp(self):
-        self.node= HTMLNode("a", "click here", None, {"href":"https://www.google.com"})
+        self.node = HTMLNode("a", "click here", None, {"href":"https://www.google.com"})
         self.node2 = HTMLNode("div", "Hello, world!", None, None)
         self.node3 = HTMLNode("img", None, None, {"src": "image.jpg", "alt": "A sample image"})
 
@@ -11,17 +11,38 @@ class TestHTMLNode(unittest.TestCase):
         self.assertEqual(self.node2.props_to_html(), "")
 
     def test_props(self):
-        self.assertEqual(self.node.props_to_html(),' href="https://www.google.com" ')
+        self.assertEqual(self.node.props_to_html(),' href="https://www.google.com"')
     
     def test_mult_props(self):
-        self.assertEqual(self.node3.props_to_html(),' src="image.jpg"  alt="A sample image" ')
+        self.assertEqual(self.node3.props_to_html(),' src="image.jpg" alt="A sample image"')
 
     def test_repr(self):
         self.assertEqual(self.node.__repr__(), "HTMLNode(\ntag = a, \nvalue = click here, \nchildren = None, \nprops = {'href': 'https://www.google.com'}\n)")
 
-    def test_to_html_err(self):
-        with self.assertRaises(NotImplementedError):
-            HTMLNode.to_html(self)
+    
+class TestLeafNode(unittest.TestCase):
+    def setUp(self):
+        self.node = LeafNode("a", "click here", {"href":"https://www.google.com"})
+        self.node2 = LeafNode("div", "Hello, world!", None)
+        self.node3 = LeafNode("img", None, {"src": "image.jpg", "alt": "A sample image"})
+        self.node4 = LeafNode("p", "Hello, world!")
+    
+    def test_to_html_no_value(self):
+        with self.assertRaises(ValueError) as context:
+            self.node3.to_html()
+        self.assertEqual(str(context.exception), "All leaf nodes must have a value")
+
+    def test_to_html_no_tag(self):
+        self.node3.tag = None
+        self.node3.value = "Insert text here" 
+        self.assertEqual(self.node3.to_html(), "Insert text here")
+
+    def test_to_html_no_children(self):
+        self.assertEqual(self.node4.to_html(), "<p>Hello, world!</p>")
+
+    def test_to_html(self):
+        self.assertEqual(self.node2.to_html(), '<div>Hello, world!</div>')
+        
 
 if __name__ == "__main__":
     unittest.main()
